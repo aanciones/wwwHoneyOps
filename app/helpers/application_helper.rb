8ae339290f2925f449a5dbb7a64e3068f1023e77
Aarón Anciones <it@championchip.cat>
2025-11-21 23:07:28 +0100

    icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -92,6 +92,24 @@ when :docs
 <circle cx="8" cy="8" r="1" />\
 <path d="M12 16v-3a2 2 0 0 1 4 0v3" />\
 <path d="M12 13h4" />'
+when :bar_chart
+  '<line x1="3" y1="3" x2="3" y2="21" />\
+<line x1="9" y1="9" x2="9" y2="21" />\
+<line x1="15" y1="5" x2="15" y2="21" />\
+<line x1="21" y1="14" x2="21" y2="21" />'
+
+when :activity
+  '<polyline points="22 12 18 12 15 21 9 3 6 12 2 12" />'
+
+when :file_text
+  '<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />\
+<polyline points="14 2 14 8 20 8" />\
+<line x1="10" y1="12" x2="14" y2="12" />\
+<line x1="10" y1="16" x2="18" y2="16" />'
+
+when :user
+  '<circle cx="12" cy="8" r="4" />\
+<path d="M6 20v-1a6 6 0 0 1 12 0v1" />'
     else
       '<circle cx="12" cy="12" r="10" />'
     end
