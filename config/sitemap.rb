32290e8cceb1214e9cc2adb9acd08dee2ffc74fe
Aarón Anciones <it@championchip.cat>
2026-01-20 10:54:44 +0100

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,15 @@
 host = ENV.fetch("CANONICAL_HOST", "honeyops.net")
 SitemapGenerator::Sitemap.default_host = "https://#{host}"
 
-def view_lastmod(path)
-  full_path = Rails.root.join(path)
-  return unless File.exist?(full_path)
-
-  File.mtime(full_path).utc
-end
-
 SitemapGenerator::Sitemap.create do
-  add "/", lastmod: view_lastmod("app/views/main/home.html.erb"), changefreq: "weekly", priority: 1.0
-  add "/cookies", lastmod: view_lastmod("app/views/legal/cookies.html.erb"), changefreq: "yearly"
-  add "/privacy", lastmod: view_lastmod("app/views/legal/privacy.html.erb"), changefreq: "yearly"
+  view_lastmod = lambda do |path|
+    full_path = Rails.root.join(path)
+    next unless File.exist?(full_path)
+
+    File.mtime(full_path).utc
+  end
+
+  add "/", lastmod: view_lastmod.call("app/views/main/home.html.erb"), changefreq: "weekly", priority: 1.0
+  add "/cookies", lastmod: view_lastmod.call("app/views/legal/cookies.html.erb"), changefreq: "yearly"
+  add "/privacy", lastmod: view_lastmod.call("app/views/legal/privacy.html.erb"), changefreq: "yearly"
 end
