95a4f1c368851a9fd5808bd858e3a5560f4222d8
Aarón Anciones <it@championchip.cat>
2026-02-10 13:57:56 +0100

    privacy and cookier error link

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,4 +1,16 @@
 class DocsController < ApplicationController
+  before_action :redirect_legal_pages
+
   def index
+  end
+
+  private
+
+  def redirect_legal_pages
+    path = request.path.sub(/\/+$/, "")
+    return unless ["/privacy", "/cookies"].include?(path)
+
+    target_host = request.host.sub(/\Adocs\./, "")
+    redirect_to "#{request.protocol}#{target_host}#{path}", allow_other_host: true
   end
 end
